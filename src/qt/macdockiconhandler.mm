8a9df23096144d78901589162f6d1fed4406354e
karthik <kartx.rockeveryone@gmail.com>
2017-10-08 21:12:57 +0000

    macOS - Added menu items to dock->right click and fixed GUI disappearing

--- src/qt/macdockiconhandler.mm
+++ src/qt/macdockiconhandler.mm
@@ -1,65 +1,130 @@
-#include "macnotificationhandler.h"
+#include "macdockiconhandler.h"
+
+#include <QImageWriter>
+#include <QMenu>
+#include <QTemporaryFile>
+#include <QWidget>
 
 #undef slots
 #include <Cocoa/Cocoa.h>
+#include <objc/objc.h>
+#include <objc/message.h>
 
-void MacNotificationHandler::showNotification(const QString &title, const QString &text)
-{
-    // check if users OS has support for NSUserNotification
-    if(this->hasUserNotificationCenterSupport()) {
-        // okay, seems like 10.8+
-        QByteArray utf8 = title.toUtf8();
-        char* cString = (char *)utf8.constData();
-        NSString *titleMac = [[NSString alloc] initWithUTF8String:cString];
+#if QT_VERSION < 0x050000
+extern void qt_mac_set_dock_menu(QMenu *);
+#endif
 
-        utf8 = text.toUtf8();
-        cString = (char *)utf8.constData();
-        NSString *textMac = [[NSString alloc] initWithUTF8String:cString];
+static MacDockIconHandler *s_instance = NULL;
 
-        // do everything weak linked (because we will keep <10.8 compatibility)
-        id userNotification = [[NSClassFromString(@"NSUserNotification") alloc] init];
-        [userNotification performSelector:@selector(setTitle:) withObject:titleMac];
-        [userNotification performSelector:@selector(setInformativeText:) withObject:textMac];
+bool dockClickHandler(id self,SEL _cmd,...) {
+    Q_UNUSED(self)
+    Q_UNUSED(_cmd)
 
-        id notificationCenterInstance = [NSClassFromString(@"NSUserNotificationCenter") performSelector:@selector(defaultUserNotificationCenter)];
-        [notificationCenterInstance performSelector:@selector(deliverNotification:) withObject:userNotification];
+    s_instance->handleDockIconClickEvent();
 
-        [titleMac release];
-        [textMac release];
-        [userNotification release];
-    }
-}
-
-//  sendAppleScript just take a QString and executes it as apple script
-void MacNotificationHandler::sendAppleScript(const QString &script)
-{
-    QByteArray utf8 = script.toUtf8();
-    char* cString = (char *)utf8.constData();
-    NSString *scriptApple = [[NSString alloc] initWithUTF8String:cString];
-
-    NSAppleScript *as = [[NSAppleScript alloc] initWithSource:scriptApple];
-    NSDictionary *err = nil;
-    [as executeAndReturnError:&err];
-    [as release];
-    [scriptApple release];
-}
-
-bool MacNotificationHandler::hasUserNotificationCenterSupport(void)
-{
-    Class possibleClass = NSClassFromString(@"NSUserNotificationCenter");
-
-    // check if users OS has support for NSUserNotification
-    if(possibleClass!=nil) {
-        return true;
-    }
+    // Return NO (false) to suppress the default OS X actions
     return false;
 }
 
+void setupDockClickHandler() {
+    Class cls = objc_getClass("NSApplication");
+    id appInst = objc_msgSend((id)cls, sel_registerName("sharedApplication"));
 
-MacNotificationHandler *MacNotificationHandler::instance()
+    if (appInst != NULL) {
+        id delegate = objc_msgSend(appInst, sel_registerName("delegate"));
+        Class delClass = (Class)objc_msgSend(delegate,  sel_registerName("class"));
+        SEL shouldHandle = sel_registerName("applicationShouldHandleReopen:hasVisibleWindows:");
+        if (class_getInstanceMethod(delClass, shouldHandle))
+            class_replaceMethod(delClass, shouldHandle, (IMP)dockClickHandler, "B@:");
+        else
+            class_addMethod(delClass, shouldHandle, (IMP)dockClickHandler,"B@:");
+    }
+}
+
+
+MacDockIconHandler::MacDockIconHandler() : QObject()
 {
-    static MacNotificationHandler *s_instance = NULL;
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+    setupDockClickHandler();
+    this->m_dummyWidget = new QWidget();
+    this->m_dockMenu = new QMenu(this->m_dummyWidget);
+    this->setMainWindow(NULL);
+#if QT_VERSION < 0x050000
+    qt_mac_set_dock_menu(this->m_dockMenu);
+#elif QT_VERSION >= 0x050200
+    this->m_dockMenu->setAsDockMenu();
+#endif
+    [pool release];
+}
+
+void MacDockIconHandler::setMainWindow(QMainWindow *window) {
+    this->mainWindow = window;
+}
+
+MacDockIconHandler::~MacDockIconHandler()
+{
+    delete this->m_dummyWidget;
+    this->setMainWindow(NULL);
+}
+
+QMenu *MacDockIconHandler::dockMenu()
+{
+    return this->m_dockMenu;
+}
+
+void MacDockIconHandler::setIcon(const QIcon &icon)
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    NSImage *image = nil;
+    if (icon.isNull())
+        image = [[NSImage imageNamed:@"NSApplicationIcon"] retain];
+    else {
+        // generate NSImage from QIcon and use this as dock icon.
+        QSize size = icon.actualSize(QSize(128, 128));
+        QPixmap pixmap = icon.pixmap(size);
+
+        // write temp file hack (could also be done through QIODevice [memory])
+        QTemporaryFile notificationIconFile;
+        if (!pixmap.isNull() && notificationIconFile.open()) {
+            QImageWriter writer(&notificationIconFile, "PNG");
+            if (writer.write(pixmap.toImage())) {
+                const char *cString = notificationIconFile.fileName().toUtf8().data();
+                NSString *macString = [NSString stringWithCString:cString encoding:NSUTF8StringEncoding];
+                image =  [[NSImage alloc] initWithContentsOfFile:macString];
+            }
+        }
+
+        if(!image) {
+            // if testnet image could not be created, load std. app icon
+            image = [[NSImage imageNamed:@"NSApplicationIcon"] retain];
+        }
+    }
+
+    [NSApp setApplicationIconImage:image];
+    [image release];
+    [pool release];
+}
+
+MacDockIconHandler *MacDockIconHandler::instance()
+{
     if (!s_instance)
-        s_instance = new MacNotificationHandler();
+        s_instance = new MacDockIconHandler();
     return s_instance;
+}
+
+void MacDockIconHandler::cleanup()
+{
+    delete s_instance;
+}
+
+void MacDockIconHandler::handleDockIconClickEvent()
+{
+    if (this->mainWindow)
+    {
+        this->mainWindow->activateWindow();
+        this->mainWindow->show();
+    }
+
+    emit this->dockIconClicked();
 }
