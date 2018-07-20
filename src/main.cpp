1f059b473f97b35fc1c96524f8be4ebd058e88d1
Dmitrij <just.run.in.the.world@yandex.ru>
2018-07-20 19:52:30 +0300

    Change PoS

--- src/main.cpp
+++ src/main.cpp
@@ -1037,9 +1037,21 @@ int64_t GetProofOfStakeReward(const CBlockIndex* pindexPrev, int64_t nCoinAge, i
     {
           nSubsidy = 0;  //default 0% yearly
     }
-        else if(pindexBest->nHeight >= GetLastPowBlock())
+        else if(pindexBest->nHeight >= GetLastPowBlock() && pindexBest->nHeight <= 564480)
     {
           nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8);  //default 100% yearly
+    }
+		else if(pindexBest->nHeight >= GetLastPowBlock() && pindexBest->nHeight <= 604800)
+    {
+          nSubsidy = nCoinAge * 65 * 33 / (365 * 33 + 8);  //default 65% yearly
+    }
+		else if(pindexBest->nHeight >= GetLastPowBlock() && pindexBest->nHeight <= 645120)
+    {
+          nSubsidy = nCoinAge * 35 * 33 / (365 * 33 + 8);  //default 35% yearly
+    }
+		else if(pindexBest->nHeight >= GetLastPowBlock() && pindexBest->nHeight > 645120)
+    {
+          nSubsidy = nCoinAge * 35 * 33 / (365 * 33 + 8);  //default 35% yearly
     }
     return nSubsidy + nFees;
 
